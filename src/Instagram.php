76b4eee9f67ed8698a29db2812e98a0575582c8a
mehmetbeyhz <mehmetbeyhz@gmail.com>
2021-06-18 21:07:39 +0300

    Instagram API

--- src/Instagram.php
+++ src/Instagram.php
@@ -144,6 +144,7 @@ class Instagram
             ->addPost('login_attempt_count',0)
             ->execute();
 
+        print_r("Response:" . $request->getResponse());
 
         $loginResponse = new LoginResponse($request->getDecodedResponse(true));
         $this->settings->set('user_id',$loginResponse->getLoggedInUser()->getPk())->save();
