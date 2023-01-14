c4c4c86e97de4e8d8a70c318d327cfdc73ca9fab
mehmetbeyhz <mehmetbeyhz@gmail.com>
2023-01-14 16:21:42 +0300

    update challenge step data.

--- src/Model/ChallengeStepData.php
+++ src/Model/ChallengeStepData.php
@@ -10,14 +10,47 @@ use EJM\MainMapper;
  * Class ChallengeStepData
  * @package IgApi\Model
  * @method getChoice()
+ * @method getDecisionClassification()
+ * @method getEnrollmentReason()
+ * @method getEnrollmentSystemContext()
+ * @method getEntrollmentTime()
+ * @method getSigpToHl()
+ * @method getSystemId()
+ * @method getTakeDownUxTemplate()
+ * @method getViolationType()
+ * @method getRenderType()
+ * @method getSecureContactPointTypeEmail()
+ * @method getTrustedEmail()
+ * @method getTrustedEmailSignature()
+ * @method getTrustedEmailOrigin()
  * @method getEmail()
+ * @method getSecureContactPointTypePhone()
+ * @method getTrustedPhoneNumber()
+ * @method getTrustedPhoneNumberOrigin()
  * @method getPhoneNumber()
  */
 class ChallengeStepData extends MainMapper
 {
     public const MAP = [
-        'choice' => 'string',
+        'decision_classification' => 'string',
+        'enrollment_reason' => 'string',
+        'enrollment_system_context' => 'string',
+        'enrollment_time' => 'string',
+        'sigp_to_hl' => 'bool',
+        'system_id' => 'string',
+        'take_down_ux_template' => 'string',
+        'violation_type' => 'string',
+        'render_type' => 'int',
+        'secure_contact_point_type_email' => 'string',
+        'trusted_email' => 'string',
+        'trusted_email_signature' => 'string',
+        'trusted_email_origin' => 'string',
+        'email' => 'string',
+        'secure_contact_point_type_phone' => 'string',
+        'trusted_phone_number' => 'string',
+        'trusted_phone_number_signature' => 'string',
+        'trusted_phone_number_origin' => 'string',
         'phone_number' => 'string',
-        'email' => 'string'
+        'choice' => 'string',
     ];
 }
